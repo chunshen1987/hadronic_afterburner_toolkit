b540618310888023cf040a662618831e28d71536
Chun Shen <chunshen1987@gmail.com>
2014-11-13 08:20:34 -0500

    change to the new output format

--- src/GNUmakefile
+++ src/GNUmakefile
@@ -11,7 +11,7 @@
 ##  
 
 CC := g++
-CFLAGS= -g $(shell gsl-config --cflags)
+CFLAGS= -O3 $(shell gsl-config --cflags)
 
 RM		=	rm -f
 O               =       .o
--- src/HBT_correlation.cpp
+++ src/HBT_correlation.cpp
@@ -385,67 +385,66 @@ void HBT_correlation::bin_into_correlation_function(int type, int num_pair, part
 
 void HBT_correlation::output_correlation_function()
 {
+    double error = 1e-4;   //"fake" error
     for(int iK = 0; iK < n_KT; iK++)
     {
-        ostringstream filename_num;
-        ostringstream filename_denorm;
-        filename_num << path << "/HBT_correlation_function_num_KT_" << KT_array[iK] << ".dat";
-        filename_denorm << path << "/HBT_correlation_function_denorm_KT_" << KT_array[iK] << ".dat";
-        ofstream output_num(filename_num.str().c_str());
-        ofstream output_denorm(filename_denorm.str().c_str());
+        ostringstream filename;
+        filename << path << "/HBT_correlation_function_KT_" << KT_array[iK] << ".dat";
+        ofstream output(filename.str().c_str());
         for(int iqlong = 0; iqlong < qnpts; iqlong++)
         {
+            double q_long_local = q_long[iqlong];
             for(int iqout = 0; iqout < qnpts; iqout++)
             {
+                double q_out_local = q_out[iqout];
                 for(int iqside = 0; iqside < qnpts; iqside++)
                 {
-                    output_num << scientific << setw(18) << setprecision(8) 
-                               << correl_3d_num[iK][iqout][iqside][iqlong] << "    ";
-                    output_denorm << scientific << setw(18) << setprecision(8) 
-                                  << correl_3d_denorm[iK][iqout][iqside][iqlong] << "    ";
+                    double q_side_local = q_side[iqside];
+                    output << scientific << setw(18) << setprecision(8) 
+                           << q_out_local << "    " << q_side_local << "    " 
+                           << q_long_local << "    "
+                           << correl_3d_num[iK][iqout][iqside][iqlong] << "    " 
+                           << correl_3d_denorm[iK][iqout][iqside][iqlong] << "    "
+                           << correl_3d_num[iK][iqout][iqside][iqlong]/correl_3d_denorm[iK][iqout][iqside][iqlong] << "    " 
+                           << error 
+                           << endl;
                 }
-                output_num << endl;
-                output_denorm << endl;
             }
-            output_num << endl;
-            output_denorm << endl;
         }
-        output_num.close();
-        output_denorm.close();
+        output.close();
     }
 }
 
 void HBT_correlation::output_correlation_function_Kphi_differential()
 {
+    double error = 1e-4;
     for(int iK = 0; iK < n_KT; iK++)
     {
         for(int iKphi = 0; iKphi < n_Kphi; iKphi++)
         {
-            ostringstream filename_num;
-            ostringstream filename_denorm;
-            filename_num << path << "/HBT_correlation_function_num_KT_" << KT_array[iK] << "_Kphi_" << Kphi_array[iKphi] << ".dat";
-            filename_denorm << path << "/HBT_correlation_function_denorm_KT_" << KT_array[iK] << "_Kphi_" << Kphi_array[iKphi] << ".dat";
-            ofstream output_num(filename_num.str().c_str());
-            ofstream output_denorm(filename_denorm.str().c_str());
+            ostringstream filename;
+            filename << path << "/HBT_correlation_function_KT_" << KT_array[iK] << "_Kphi_" << Kphi_array[iKphi] << ".dat";
+            ofstream output(filename.str().c_str());
             for(int iqlong = 0; iqlong < qnpts; iqlong++)
             {
+                double q_long_local = q_long[iqlong];
                 for(int iqout = 0; iqout < qnpts; iqout++)
                 {
+                    double q_out_local = q_out[iqout];
                     for(int iqside = 0; iqside < qnpts; iqside++)
                     {
-                        output_num << scientific << setw(18) << setprecision(8) 
-                                   << correl_3d_Kphi_diff_num[iK][iKphi][iqout][iqside][iqlong] << "    ";
-                        output_denorm << scientific << setw(18) << setprecision(8) 
-                                      << correl_3d_Kphi_diff_denorm[iK][iKphi][iqout][iqside][iqlong] << "    ";
+                        double q_side_local = q_side[iqside];
+                        output << scientific << setw(18) << setprecision(8) 
+                               << q_out_local << "    " << q_side_local << "    "
+                               << q_long_local << "    "
+                               << correl_3d_Kphi_diff_num[iK][iKphi][iqout][iqside][iqlong] << "    "
+                               << correl_3d_Kphi_diff_denorm[iK][iKphi][iqout][iqside][iqlong] << "    "
+                               << correl_3d_Kphi_diff_num[iK][iKphi][iqout][iqside][iqlong]/correl_3d_Kphi_diff_denorm[iK][iKphi][iqout][iqside][iqlong] << "    "
+                               << error << endl;
                     }
-                    output_num << endl;
-                    output_denorm << endl;
                 }
-                output_num << endl;
-                output_denorm << endl;
             }
-            output_num.close();
-            output_denorm.close();
+            output.close();
         }
     }
 }
