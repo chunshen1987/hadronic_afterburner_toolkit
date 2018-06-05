5869ed8dae80987f8d49c32f2a4cc1d192eabaee
Chun Shen <chunshen1987@gmail.com>
2018-06-05 14:47:59 -0400

    more std

--- src/parameters.h
+++ src/parameters.h
@@ -1,9 +1,6 @@
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
-#include<string>
-using namespace std;
-
 const double hbarC=0.197327053;  //GeV*fm
 
 const int Maxparticle=400;            //size of array for storage of the particles
