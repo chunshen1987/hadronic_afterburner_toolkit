e45583c4b887c34a8ae106d2f28d67abba00d4e0
Chun Shen <chunshen1987@gmail.com>
2018-10-05 14:54:35 -0700

    update the analysis script for balance function

--- ebe_scripts/average_event_balance_function.py
+++ ebe_scripts/average_event_balance_function.py
@@ -116,6 +116,51 @@ for ipart in pair_list:
     shutil.move(file_name, avg_folder)
     savetxt(file_name_err, event_avg_data_err, fmt='%.10e', delimiter='  ')
     shutil.move(file_name_err, avg_folder)
+    
+    
+    file_name     = 'Correlation_function_%s_os_2D.dat' % ipart
+    file_name_err = 'Correlation_function_%s_os_2D_err.dat' % ipart
+    event_avg_data = loadtxt(
+            path.join(file_folder_list[0],
+                      'Correlation_function_9998_-9998_os_2D.dat'))*0.0
+    event_avg_data_err = 0.0*event_avg_data
+    for ifolder in range(nev):
+        results_folder = path.abspath(file_folder_list[ifolder])
+        print("processing %s/%s ..." % (results_folder, file_name))
+        temp_data = loadtxt(path.join(results_folder, file_name))
+        event_avg_data     += temp_data
+        event_avg_data_err += temp_data**2.
+
+    event_avg_data     = event_avg_data/nev
+    event_avg_data_err = event_avg_data_err/nev
+    event_avg_data_err = sqrt((event_avg_data_err - event_avg_data**2.)/nev)
+
+    savetxt(file_name, event_avg_data, fmt='%.10e', delimiter='  ')
+    shutil.move(file_name, avg_folder)
+    savetxt(file_name_err, event_avg_data_err, fmt='%.10e', delimiter='  ')
+    shutil.move(file_name_err, avg_folder)
+    
+    file_name     = 'Correlation_function_%s_ss_2D.dat' % ipart
+    file_name_err = 'Correlation_function_%s_ss_2D_err.dat' % ipart
+    event_avg_data = loadtxt(
+            path.join(file_folder_list[0],
+                      'Correlation_function_9998_-9998_ss_2D.dat'))*0.0
+    event_avg_data_err = 0.0*event_avg_data
+    for ifolder in range(nev):
+        results_folder = path.abspath(file_folder_list[ifolder])
+        print("processing %s/%s ..." % (results_folder, file_name))
+        temp_data = loadtxt(path.join(results_folder, file_name))
+        event_avg_data     += temp_data
+        event_avg_data_err += temp_data**2.
+
+    event_avg_data     = event_avg_data/nev
+    event_avg_data_err = event_avg_data_err/nev
+    event_avg_data_err = sqrt((event_avg_data_err - event_avg_data**2.)/nev)
+
+    savetxt(file_name, event_avg_data, fmt='%.10e', delimiter='  ')
+    shutil.move(file_name, avg_folder)
+    savetxt(file_name_err, event_avg_data_err, fmt='%.10e', delimiter='  ')
+    shutil.move(file_name_err, avg_folder)
 
 print("Analysis is done.")
 
