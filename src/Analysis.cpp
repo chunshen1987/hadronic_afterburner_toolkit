375b5cb53b7b21836e9ea89689d36e311a088b4e
Wenbin <1501110084@pku.edu.cn>
2024-07-11 23:54:20 -0700

    Add more cut for ATLAS

--- src/Analysis.cpp
+++ src/Analysis.cpp
@@ -136,6 +136,14 @@ void Analysis::FlowAnalysis() {
     spvn.push_back(new singleParticleSpectra(paraRdr_, path_, ran_gen_ptr_));
     paraRdr_.setVal("rap_min", -2.5); paraRdr_.setVal("rap_max", 2.5); // The ATLAS cut 
     spvn.push_back(new singleParticleSpectra(paraRdr_, path_, ran_gen_ptr_));
+    paraRdr_.setVal("rap_min", -1.6); paraRdr_.setVal("rap_max", -0.8);
+    spvn.push_back(new singleParticleSpectra(paraRdr_, path_, ran_gen_ptr_));
+    paraRdr_.setVal("rap_min", -0.8); paraRdr_.setVal("rap_max", 0.0);
+    spvn.push_back(new singleParticleSpectra(paraRdr_, path_, ran_gen_ptr_));
+    paraRdr_.setVal("rap_min", -0.0); paraRdr_.setVal("rap_max", 0.8);
+    spvn.push_back(new singleParticleSpectra(paraRdr_, path_, ran_gen_ptr_));
+    paraRdr_.setVal("rap_min",  0.8); paraRdr_.setVal("rap_max", 1.6);
+    spvn.push_back(new singleParticleSpectra(paraRdr_, path_, ran_gen_ptr_));
     paraRdr_.setVal("rap_min", -0.8); paraRdr_.setVal("rap_max", 0.8);
     spvn.push_back(new singleParticleSpectra(paraRdr_, path_, ran_gen_ptr_));
     paraRdr_.setVal("rap_min", -0.5); paraRdr_.setVal("rap_max", 0.5);
